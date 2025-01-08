780ce6127bdd701166b3cd116585359d11b101d9
mariontessa <xanfonmarion@gmail.com>
2025-01-08 10:22:31 +0300

    PORT env

--- src/init.js
+++ src/init.js
@@ -1,12 +1,12 @@
-import "regenerator-runtime"
+import "regenerator-runtime";
 import "dotenv/config";
 import "./db";
 import "./models/video";
 import "./models/user";
-import"./models/comment";
+import "./models/comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ server listening on port http://localhost:${PORT}`);
